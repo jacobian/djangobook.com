9be22d37354d947bf0af1e851aa4af9f40d1e8d1
Jacob Kaplan-Moss <jacob@jacobian.org>
2012-09-25 07:47:11 -0500

    Updated the little serving app to send out redirects for the old site.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w4 -b0.0.0.0:$PORT app:app
+web: gunicorn -kgevent -w4 -b0.0.0.0:$PORT app:app
--- app.py
+++ app.py
@@ -1,4 +1,37 @@
-import os
-import static
+"""
+The simplest possible app that can serve these Sphinx docs and issue
+redirects for the previous URLs.
+"""
 
-app = static.Cling(os.path.join(os.path.dirname(__file__), '_build/html'))
+import static
+import selector
+from unipath import FSPath as Path
+
+app = selector.Selector()
+
+def redirect(to):
+    """
+    Create a 301 redirect WSGI app.
+
+    `to` may contain str.format-style formatting which'll be formatted against
+    the routing arguments (wsgiorg.routing_args).
+    """
+    def _redirect(environ, start_response):
+        args, kwargs = environ['wsgiorg.routing_args']
+        start_response('301 MOVED PERMANENTLY',
+                       [('Location', to.format(*args, **kwargs))])
+        return []
+    return _redirect
+
+# Redirects for old doc URLs, since Cool URIs Don't Change.
+app.add('/',                    GET=redirect('/en/2.0/index.html'))
+app.add('/license/',            GET=redirect('/en/2.0/license.html'))
+app.add('/about/',              GET=redirect('/en/2.0/frontmatter.html'))
+app.add('/en/1.0/',             GET=redirect('/en/2.0/index.html'))
+app.add('/en/1.0/{doc:chunk}/', GET=redirect('/en/2.0/{doc}.html'))
+app.add('/en/2.0/{doc:chunk}/', GET=redirect('/en/2.0/{doc}.html'))
+
+# Serve docs at "/en/2.0" still, to leave room for the future.
+# One of these days I'll actually do this, not just talk about it!
+docs = static.Cling(Path(__file__).parent.child('_build', 'html'))
+app.add("/en/2.0|", GET=docs)
